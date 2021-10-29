376071a67a5094d290733ddc5e91efc319254d3f
djungeldan <79529185+djungeldan@users.noreply.github.com>
2021-10-29 12:56:48 +0100

    Update filetest.py
    File manipulation script for the counter text file. Doing it this way so its easy for python and PHP to talk to eachother

--- filetest.py
+++ filetest.py
@@ -1,16 +1,41 @@
-with open("counter.txt", 'r') as f:
-    for line in f:
-        if f.read == 5:
-            print("Was 5")
-            f.close()
-        else:
-            with open("counter.txt","w") as d:
-                    contents = f.read()
-                    contents = int(contents)
-                    contents =+ 1
-                    print(contents)
-                    d.write(str(contents))
-                    print(f.read())
+'''
+RIGHT THIS IS GONNA WORK
+this is a file for file manipulation testing
+for some reason ive been having issues with reading a value from the txt
+some data type issue
+gonna start from scratch and this should work 
+'''
+reset = 0
+with open("counter.txt","r+") as getCounter:
+
+    total = 1
+
+    for line in getCounter:
+
+        try:
+
+            total += int(line)
+
+            with open("counter.txt","w+") as writeCounter:
+
+                writeCounter.writelines(str(total))
+
+                if total >= 5:
+                    print("Counter is either 5 or more than 5")
+                    writeCounter.close()
+
+                    with open("counter.txt","w") as resetLines:
+                        resetLines.write(str(reset))
+                        break
+
+                else:
+                    print("Counter is less than 5")
+
+        except ValueError:
+            pass
+
+
+
 
 
 
